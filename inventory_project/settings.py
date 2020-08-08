13fb8f7f3edf7eda0b1ab2b2fcec26783ef30ecb
Johannes Schriewer <hallo@dunkelstern.de>
2020-08-08 02:19:44 +0200

    Serve media files

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -134,3 +134,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+SERVE_MEDIA_FILES = DEBUG
--- inventory_project/urls.py
+++ inventory_project/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('inventory.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.SERVE_MEDIA_FILES:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

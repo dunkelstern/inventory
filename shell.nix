5d175878290b2210f3ed1202fa79d44745656407
Johannes Schriewer <hallo@dunkelstern.de>
2023-12-26 00:45:31 +0100

    Add shell.nix and update deps

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  pypkgs-build-requirements = {
+    sqlparse = [ "flit-core" ];
+    python-monkey-business = [ "setuptools" ];
+    # typed-ast = [ "setuptools" ];
+    # typing_extensions = [ "setuptools" ];
+  };
+  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
+    builtins.mapAttrs (package: build-requirements:
+      (builtins.getAttr package super).overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+      })
+    ) pypkgs-build-requirements
+  );
+
+  inventoryAppEnv = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    python = pkgs.python311;
+    preferWheels = true;
+    overrides = p2n-overrides;
+    editablePackageSources = {
+      inventory = ./inventory;
+    };
+  };
+in inventoryAppEnv.env.overrideAttrs (oldAttrs: {
+  buildInputs = with pkgs ; [ 
+    postgresql
+  ];
+})

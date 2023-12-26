be2f277eb38f3c3403f7d9946ff1f21824748722
Johannes Schriewer <hallo@dunkelstern.de>
2023-12-26 01:19:59 +0100

    update asgi starter to 5.0

--- inventory_project/asgi.py
+++ inventory_project/asgi.py
@@ -4,15 +4,13 @@ ASGI config for inventory_project project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings')
 
-
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-})
+application = get_asgi_application()

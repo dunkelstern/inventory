3ca926afacd0892a6047872f9fecad2c5e5d8b6b
Johannes Schriewer <hallo@dunkelstern.de>
2025-01-05 21:45:44 +0100

    Manufacturer detail view

--- inventory/models/manufacturer.py
+++ inventory/models/manufacturer.py
@@ -14,3 +14,6 @@ class Manufacturer(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta:
+        ordering = ("name", )
--- inventory/views/manufacturer.py
+++ inventory/views/manufacturer.py
@@ -1,13 +1,24 @@
-from django.views import View
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 
-from inventory.models import Manufacturer
+from inventory.models import Manufacturer, Item
 
 
-class ManufacturerView(View):
-    pass
+@method_decorator(login_required, name='dispatch')
+class ManufacturerView(DetailView):
+    context_object_name = 'manufacturer'
+    queryset = Manufacturer.objects.all().prefetch_related('tags')
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        result = super().get_context_data(*args, object_list=object_list, **kwargs)
+        p = self.request.GET.get("item_page", 1)
+        paginator = Paginator(Item.objects.filter(manufacturer=self.get_object()).select_related('container', 'distributor').order_by('name'), 50)
+        result.update({
+            "items": paginator.get_page(p)
+        })
+        return result
 
 
 @method_decorator(login_required, name='dispatch')

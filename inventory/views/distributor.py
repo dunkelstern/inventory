4b2e8ccabbc31286f6630a4a111b08b1fad3ce4b
Johannes Schriewer <hallo@dunkelstern.de>
2025-01-05 19:03:53 +0100

    Distributor lists

--- inventory/views/distributor.py
+++ inventory/views/distributor.py
@@ -1,9 +1,15 @@
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView
+
+from inventory.models import Distributor
 
 
 class DistributorView(View):
     pass
 
 
-class DistributorListView(View):
-    pass
+@method_decorator(login_required, name='dispatch')
+class DistributorListView(ListView):
+    model = Distributor

37388ba7be2094ceccc05081066f81967478f86b
Johannes Schriewer <hallo@dunkelstern.de>
2025-01-05 19:04:02 +0100

    Manufacturer lists

--- inventory/views/manufacturer.py
+++ inventory/views/manufacturer.py
@@ -1,9 +1,15 @@
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView
+
+from inventory.models import Manufacturer
 
 
 class ManufacturerView(View):
     pass
 
 
-class ManufacturerListView(View):
-    pass
+@method_decorator(login_required, name='dispatch')
+class ManufacturerListView(ListView):
+    model = Manufacturer
